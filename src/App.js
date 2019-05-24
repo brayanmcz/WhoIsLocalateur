855f34b3df95616a8bb62a664e74b90db7fed175
vdauverg <vicentedauvergne@yahoo.ie>
2019-05-24 04:13:48 -0700

    Additional work on feature-cards

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ function App() {
       <LandingSection />
       <FeatureCardLeft />
       <FeatureCardRight />
+      <FeatureCardLeft />
+      <FeatureCardRight />
     </Wrapper>
   );
 }
--- src/shared/feature-card.js
+++ src/shared/feature-card.js
@@ -7,33 +7,50 @@ import {
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-    // height: 150px;
-    margin-bottom: 50px;
+    // height: 300px;
+    margin-top: 25px;
+    margin-bottom: 100px;
     margin-left: 15px;
     margin-right: 15px;
 
-    .image-container-left {
-        height: 150px;
+    .image-container {
+        height: 300px;
         padding: 0px;
-    }
-
-    .image-container-right {
-        height: 150px;
-        padding: 0px;
-        text-align: right;
+        text-align: center;
     }
 
     .image {
-        // border: 1px solid black;
         border-radius: 5px;
         width: 90%;
-        height: 100%;
+        height: 300px;
+        object-fit: cover;
+        margin-top: auto;
+        margin-bottom: auto;
         box-shadow: 2px 5px 7px 3px lightgrey;
     }
 
     .title {
-        font-size: 22px;
-        font-weight: bold;
+        font-size: 28px !important;
+        font-weight: 800 !important;
+        color: #686868 !important;
+        font-family: Impact !important;
+        margin-top: 15% !important;
+    }
+
+    .text {
+        font-family: Franklin Gothic Medium;
+        font-size: 18px;
+        margin-top: 10px;
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 500px;
+        text-align: center;
+        font-weight: 600;
+        color: #9e9c9c;
+    }
+
+    .right {
+        box-shadow: -2px 5px 7px 3px lightgrey !important;
     }
 `;
 
@@ -43,15 +60,21 @@ export class FeatureCardLeft extends Component {
             <Wrapper>
                 <MDBContainer>
                     <MDBRow>
-                        <MDBCol size="12" sm="3" className="image-container-left">
+                        <MDBCol size="12" sm="4" className="image-container">
                             <img className="image" src="https://cdn-image.foodandwine.com/sites/default/files/1501607996/opentable-scenic-restaurants-marine-room-FT-BLOG0818.jpg" />
                         </MDBCol>
-                        <MDBCol size="12" sm="9">
-                            <MDBRow className="title">
-                                TELL YOUR STORY
+                        <MDBCol size="12" sm="8">
+                            <MDBRow className="title text">
+                                <MDBCol>
+                                    TELL YOUR STORY
+                                </MDBCol>
                             </MDBRow>
-                            <MDBRow clasName="text">
-                                Share your origins and journeys with the world, and teach customers about your culture!
+                            <MDBRow className="text">
+                                <MDBCol>
+                                    Share your origins and journeys with the world, and teach customers about your culture!
+                                    Le quoi de la meme, once upon a time there was a toaster,
+                                    he did not like mems very much, bubt one day he encountered a wild wasp.
+                                </MDBCol>
                             </MDBRow>
                         </MDBCol>
                     </MDBRow>
@@ -67,16 +90,22 @@ export class FeatureCardRight extends Component {
             <Wrapper>
                 <MDBContainer>
                     <MDBRow>
-                        <MDBCol size="12" sm="9">
-                            <MDBRow className="title">
-                                TELL YOUR STORY
+                    <MDBCol size="12" sm="8">
+                            <MDBRow className="title text">
+                                <MDBCol>
+                                    TELL YOUR STORY
+                                </MDBCol>
                             </MDBRow>
                             <MDBRow className="text">
-                                Share your origins and journeys with the world, and teach customers about your culture!
+                                <MDBCol>
+                                    Share your origins and journeys with the world, and teach customers about your culture!
+                                    Le quoi de la meme, once upon a time there was a toaster,
+                                    he did not like mems very much, bubt one day he encountered a wild wasp.
+                                </MDBCol>
                             </MDBRow>
                         </MDBCol>
-                        <MDBCol size="12" sm="3" className="image-container-right">
-                            <img className="image" src="https://cdn-image.foodandwine.com/sites/default/files/1501607996/opentable-scenic-restaurants-marine-room-FT-BLOG0818.jpg" />
+                        <MDBCol size="12" sm="4" className="image-container">
+                            <img className="image right" src="https://cdn-image.foodandwine.com/sites/default/files/1501607996/opentable-scenic-restaurants-marine-room-FT-BLOG0818.jpg" />
                         </MDBCol>
                     </MDBRow>
                 </MDBContainer>
